13e7a7f77765e4a3e90116823918a9949d139a47
twotwo <twotwo.li@gmail.com>
2018-02-22 17:58:02 +0800

    1. 修改nginx配置 2. 跑通基本路由逻辑

--- README.md
+++ README.md
@@ -17,4 +17,11 @@ http://ci-dev.com/news/latest/10
 
 ### [加载静态内容](http://codeigniter.org.cn/user_guide/tutorial/static_pages.html)
 
-http://ci-dev.com/index.php/[controller-class]/[controller-method]/[arguments] //未生效
+路由配置： application/config/routes.php
+
+访问：
+
+http://ci-dev.com/index.php/[controller-class]/[controller-method]/[arguments]
+
+http://ci-dev.com/pages/view/home
+http://ci-dev.com/pages/view/about
--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -2,10 +2,10 @@
 class Pages extends CI_Controller {
 
     public function view($page = 'home') {
-        // if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php')) {
-        //         // Whoops, we don't have a page for that!
-        //         show_404();
-        // }
+        if ( ! file_exists(APPPATH.'views/pages/'.$page.'.php')) {
+                // Whoops, we don't have a page for that!
+                show_404();
+        }
 
         $data['title'] = ucfirst($page); // Capitalize the first letter
 

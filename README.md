66fccdfcdf10e45ce575a869b90351a8f988e5ff
twotwo <twotwo.li@gmail.com>
2018-02-22 18:58:06 +0800

    继续写代码，完成读取新闻条目: 1. 配置数据库 2. 显示新闻

--- README.md
+++ README.md
@@ -25,3 +25,8 @@ http://ci-dev.com/index.php/[controller-class]/[controller-method]/[arguments]
 
 http://ci-dev.com/pages/view/home
 http://ci-dev.com/pages/view/about
+
+### [读取新闻条目](http://codeigniter.org.cn/user_guide/tutorial/news_section.html)
+
+* 数据库配置: dbdriver要设置成mysqli
+* 显示数据: http://ci-dev.com/news
--- application/config/database.php
+++ application/config/database.php
@@ -48,11 +48,11 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
-$db['default']['dbdriver'] = 'mysql';
+$db['default']['hostname'] = '106.75.19.156';
+$db['default']['username'] = 'node';
+$db['default']['password'] = 'pD#5T~l14+,i';
+$db['default']['database'] = 'test';
+$db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
